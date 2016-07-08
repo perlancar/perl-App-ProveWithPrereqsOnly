1e5f432d6ea46b2d617114ebf18b63547cc785e1
perlancar (@pc-home) <perlancar@gmail.com>
2016-07-08 17:56:32 +0700

    Initial commit

---
+++ lib/App/ProveWithPrereqsOnly.pm
@@ -0,0 +1,18 @@
+package App::ProveWithPrereqsOnly;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT:
+
+=head1 SYNOPSIS
+
+This distribution includes the following CLI utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<prove>, L<App::prove>, L<lib::prereqs::only>
